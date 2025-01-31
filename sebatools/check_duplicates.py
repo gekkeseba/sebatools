86defa5b0093ad6deb8e711b48b69fd64667e84f
Sebastiaan Hendriks <sebastiaan.hendriks.ext@MacBookPro.home>
2025-01-31 13:01:23 +0100

    updated check duplicates to import spark session

--- sebatools/check_duplicates.py
+++ sebatools/check_duplicates.py
@@ -1,4 +1,9 @@
+from pyspark.sql import SparkSession
+
 def check_duplicates(table_name, columns):
+    spark = SparkSession.getActiveSession()
+    if not spark:
+        spark = SparkSession.builder.getOrCreate()
     df = spark.table(table_name)
     duplicates = df.groupBy(columns).count().filter("count > 1")
     duplicate_entries = df.join(duplicates, on=columns, how='inner')
