92f30ca226ca322392f87ce7c7f71556a4649f33
Sebastiaan Hendriks <sebastiaan.hendriks.ext@MacBookPro.home>
2025-01-31 13:20:29 +0100

    adjusted again, updated version

--- sebatools/check_duplicates.py
+++ sebatools/check_duplicates.py
@@ -1,11 +1,12 @@
-from pyspark.sql import SparkSession
-
-def check_duplicates(table_name, columns):
-    spark = SparkSession.getActiveSession()
-    if not spark:
-        spark = SparkSession.builder.getOrCreate()
+def check_duplicates(spark, table_name, columns):
     df = spark.table(table_name)
     duplicates = df.groupBy(columns).count().filter("count > 1")
     duplicate_entries = df.join(duplicates, on=columns, how='inner')
-    display(duplicate_entries)
+    
+    # Check if the display function is available
+    if 'display' in globals():
+        display(duplicate_entries)
+    else:
+        duplicate_entries.show()  # Use show() as a fallback
+    
     return duplicates.count() > 0
